ae65c0162cac95a3c990e9893fd608b53a23fcab
hack0303 <1097037585@qq.com>
2018-11-23 10:36:30 +0800

    Multiple Params List

---
+++ src/com/creating/www/multiple/params/MultipleParamsTest.scala
@@ -0,0 +1,12 @@
+package com.creating.www.multiple.params
+
+object MultipleParamsTest extends App {
+  //def left[B](i:B)(op:(B,B)=>B):B
+  def list=List(1,2,3,4)
+  println(list.foldLeft(0)((m,n)=>m+n))
+  println(list.foldRight(0)((m,n)=>m+n))
+  println(list.foldLeft(0)(_+_))
+  println(list.foldRight(0)(_+_))
+  
+  //=>自定义和传函数的方式等后面再补全
+}
